8dadc51f59ae9fa3bb9246eec42872a5fe4c719e
Vincent Lin <vincent.open.source.taiwan@gmail.com>
2024-02-12 10:31:12 +0800

    setup form

--- lib/liveview_form_text_input_web/live/home.ex
+++ lib/liveview_form_text_input_web/live/home.ex
@@ -4,6 +4,10 @@ defmodule LiveviewFormTextInputWeb.Home do
   def render(assigns) do
     ~H"""
     <h1>Welcome to Phoenix!</h1>
+    <form phx-submit="submit">
+      <input type="text" name="name" />
+      <button type="button">Click Me!</button>
+    </form>
     """
   end
 
